4a415ad67fc8d52766b01b247a6a3dc65a29cb54
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2024-12-13 21:52:37 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/Shyamala9926/todoList-App.git', // Replace <repository-name> with your GitHub repo name
+  base: '/todoList', // Replace <repository-name> with your GitHub repo name
   plugins: [react()],
 });
+
+/*import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
+
+export default defineConfig({
+  plugins: [react(), ghPages()],
+  base: '/todoList-App', // Replace with your repo name
+});*/
+
